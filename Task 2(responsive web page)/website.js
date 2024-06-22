b62ec151ea4260f5a80c3f0ff4064a2cc8d286cf
gowthamikunte14 <162871735+gowthamikunte14@users.noreply.github.com>
2024-06-22 21:13:47 +0530

    Task 2 ( Responsive  webpage)
    As it is the second task of web development, created a responsive webpage with additional features in it. As it is created with the help of Html, CSS and little java script. As you can see here :      file:///C:/Users/K%20Gowthami/OneDrive/Desktop/mainflow%20intern/Task%202(responsive%20web%20page)/index.html

---
+++ Task 2(responsive web page)/website.js
@@ -0,0 +1,6 @@
+const learnMoreBtn = document.getElementById('learn-more-btn');
+const signInBox = document.getElementById('sign-in-box');
+
+learnMoreBtn.addEventListener('click', () => {
+  signInBox.style.display = 'block';
+});
